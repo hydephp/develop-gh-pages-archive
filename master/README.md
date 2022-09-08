c335c0dbf0e1a7ca9399fc733e614454853adcba
caendesilva <caendesilva@users.noreply.github.com>
2022-09-08 15:54:11 +0000

    Upload live reports from test suite run ca30a2f91b8468a3a28e25d4bf28591f2408a51d

--- master/README.md
+++ master/README.md
@@ -1,1 +1,1 @@
-# CI/CD generated reports for [f4663c2891c5e22db7cbaaddc66b02033b9ede85](https://github.com/hydephp/develop/commit/f4663c2891c5e22db7cbaaddc66b02033b9ede85)
+# CI/CD generated reports for [ca30a2f91b8468a3a28e25d4bf28591f2408a51d](https://github.com/hydephp/develop/commit/ca30a2f91b8468a3a28e25d4bf28591f2408a51d)
--- master/api-docs/js/searchIndex.js
+++ master/api-docs/js/searchIndex.js
@@ -1746,41 +1746,6 @@ Search.appendIndex(
             "summary": "",
             "url": "classes/Hyde-Framework-Facades-Asset.html#method_getFacadeAccessor"
         },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes",
-            "name": "Includes",
-            "summary": "",
-            "url": "classes/Hyde-Framework-Facades-Includes.html"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003Apath\u0028\u0029",
-            "name": "path",
-            "summary": "Return\u0020the\u0020path\u0020to\u0020the\u0020includes\u0020directory,\u0020or\u0020a\u0020partial\u0020within\u0020it,\u0020if\u0020requested.",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#method_path"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003Aget\u0028\u0029",
-            "name": "get",
-            "summary": "Get\u0020the\u0020raw\u0020contents\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#method_get"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003Amarkdown\u0028\u0029",
-            "name": "markdown",
-            "summary": "Get\u0020the\u0020rendered\u0020Markdown\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#method_markdown"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003Ablade\u0028\u0029",
-            "name": "blade",
-            "summary": "Get\u0020the\u0020rendered\u0020Blade\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#method_blade"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003AneedsDirectory\u0028\u0029",
-            "name": "needsDirectory",
-            "summary": "",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#method_needsDirectory"
-        },                {
-            "fqsen": "\\Hyde\\Framework\\Facades\\Includes\u003A\u003A\u0024includesDirectory",
-            "name": "includesDirectory",
-            "summary": "",
-            "url": "classes/Hyde-Framework-Facades-Includes.html#property_includesDirectory"
-        },                {
             "fqsen": "\\Hyde\\Framework\\Foundation\\Application",
             "name": "Application",
             "summary": "",
@@ -2040,6 +2005,41 @@ Search.appendIndex(
             "name": "toArray",
             "summary": "",
             "url": "classes/Hyde-Framework-Helpers-Features.html#method_toArray"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes",
+            "name": "Includes",
+            "summary": "",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003Apath\u0028\u0029",
+            "name": "path",
+            "summary": "Return\u0020the\u0020path\u0020to\u0020the\u0020includes\u0020directory,\u0020or\u0020a\u0020partial\u0020within\u0020it,\u0020if\u0020requested.",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#method_path"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003Aget\u0028\u0029",
+            "name": "get",
+            "summary": "Get\u0020the\u0020raw\u0020contents\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#method_get"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003Amarkdown\u0028\u0029",
+            "name": "markdown",
+            "summary": "Get\u0020the\u0020rendered\u0020Markdown\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#method_markdown"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003Ablade\u0028\u0029",
+            "name": "blade",
+            "summary": "Get\u0020the\u0020rendered\u0020Blade\u0020of\u0020a\u0020partial\u0020file\u0020in\u0020the\u0020includes\u0020directory.",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#method_blade"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003AneedsDirectory\u0028\u0029",
+            "name": "needsDirectory",
+            "summary": "",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#method_needsDirectory"
+        },                {
+            "fqsen": "\\Hyde\\Framework\\Helpers\\Includes\u003A\u003A\u0024includesDirectory",
+            "name": "includesDirectory",
+            "summary": "",
+            "url": "classes/Hyde-Framework-Helpers-Includes.html#property_includesDirectory"
         },                {
             "fqsen": "\\Hyde\\Framework\\Helpers\\Meta",
             "name": "Meta",
