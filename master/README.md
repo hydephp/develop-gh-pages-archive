d08f3ee87352f0452b2b4a720c169e7d0466c05a
caendesilva <caendesilva@users.noreply.github.com>
2022-12-14 17:56:30 +0000

    Upload live reports from test suite run 39f3e5392edcce48fc4d49e0119f8d61d462a190

--- master/README.md
+++ master/README.md
@@ -1,1 +1,1 @@
-# CI/CD generated reports for [8f0998d29009a560430d2313c80468a3e79fe0db](https://github.com/hydephp/develop/commit/8f0998d29009a560430d2313c80468a3e79fe0db)
+# CI/CD generated reports for [39f3e5392edcce48fc4d49e0119f8d61d462a190](https://github.com/hydephp/develop/commit/39f3e5392edcce48fc4d49e0119f8d61d462a190)
--- master/api-docs/js/searchIndex.js
+++ master/api-docs/js/searchIndex.js
@@ -4133,7 +4133,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\Hyde\\Framework\\Services\\ChecksumService",
             "name": "ChecksumService",
-            "summary": "Helper\u0020methods\u0020to\u0020interact\u0020with\u0020the\u0020filecache.\u0020The\u0020filecache\u0020is\u0020used\u0020to\u0020compare\npublished\u0020Blade\u0020views\u0020with\u0020the\u0020original\u0020Blade\u0020views\u0020in\u0020the\u0020Hyde\u0020Framework\nso\u0020the\u0020user\u0020can\u0020be\u0020warned\u0020before\u0020overwriting\u0020their\u0020customizations.",
+            "summary": "Helper\u0020methods\u0020to\u0020interact\u0020with\u0020the\u0020virtual\u0020filecache\u0020that\u0020is\u0020used\u0020to\u0020compare\npublished\u0020Blade\u0020views\u0020with\u0020the\u0020original\u0020Blade\u0020views\u0020in\u0020the\u0020Hyde\u0020Framework\nso\u0020the\u0020user\u0020can\u0020be\u0020warned\u0020before\u0020overwriting\u0020their\u0020customizations.",
             "url": "classes/Hyde-Framework-Services-ChecksumService.html"
         },                {
             "fqsen": "\\Hyde\\Framework\\Services\\ChecksumService\u003A\u003AgetFilecache\u0028\u0029",
@@ -4151,9 +4151,14 @@ Search.appendIndex(
             "summary": "",
             "url": "classes/Hyde-Framework-Services-ChecksumService.html#method_checksumMatchesAny"
         },                {
+            "fqsen": "\\Hyde\\Framework\\Services\\ChecksumService\u003A\u003Aunixsum\u0028\u0029",
+            "name": "unixsum",
+            "summary": "A\u0020EOL\u0020agnostic\u0020wrapper\u0020for\u0020calculating\u0020MD5\u0020checksums.",
+            "url": "classes/Hyde-Framework-Services-ChecksumService.html#method_unixsum"
+        },                {
             "fqsen": "\\Hyde\\Framework\\Services\\ChecksumService\u003A\u003AunixsumFile\u0028\u0029",
             "name": "unixsumFile",
-            "summary": "",
+            "summary": "Shorthand\u0020for\u0020\u007B\u0040see\u0020static\u003A\u003Aunixsum\u0028\u0029\u007D\u0020but\u0020loads\u0020a\u0020file.",
             "url": "classes/Hyde-Framework-Services-ChecksumService.html#method_unixsumFile"
         },                {
             "fqsen": "\\Hyde\\Framework\\Services\\DiscoveryService",
